d97d8b017c1cf3c4cc01706643a2286245cc5b87
lorenzoporc <lorenzoporc@gmail.com>
2021-04-17 01:49:00 +0200

    uppone

--- matlab/setup.m
+++ matlab/setup.m
@@ -11,6 +11,7 @@ set(0, 'defaultLegendInterpreter','latex');
 
 %% Constants
 constants.U = 0.1; %[m]
+constants.mu = 398600; %[km^3/s^2]
 
 %% Spacecraft setup
 spacecraft = struct();
@@ -48,7 +49,7 @@ spacecraft.r_CoM = (m_cube*sum(spacecraft.r_CoM_cubes) + ...
 spacecraft.r_CoM_cubes = spacecraft.r_CoM_cubes - spacecraft.r_CoM;
 spacecraft.r_CoM_panels = spacecraft.r_CoM_panels - spacecraft.r_CoM;
 
-plotLogic = true;
+plotLogic = false;
 
 if plotLogic
     figure(1)
@@ -94,15 +95,24 @@ surfaces.panels.num = 2;
 surfaces.body.n = [ny; nx; -ny; -nx; nz; -nz];
 surfaces.panels.n = [ny; ny; - ny; -ny];
 
-A_2constants.U = 2*constants.U*constants.U;
-A_6constants.U = 3*constants.U*2*constants.U;
-A_3constants.U = 3*constants.U*constants.U;
+A_2U = 2*constants.U*constants.U;
+A_3U = 3*constants.U*constants.U;
+A_6U = 3*constants.U*2*constants.U;
 A_panel = surfaces.panels.width*surfaces.panels.length;
 
-surfaces.body.A = [A_2constants.U; A_6constants.U; A_2constants.U; A_6constants.U; A_3constants.U; A_3constants.U];
+surfaces.body.A = [A_2U; A_6U; A_2U; A_6U; A_3U; A_3U];
 surfaces.panels.A = A_panel*ones(4,1);
 
 %% Drag and optical properties
+
+surfaces.body.r_surf = [0, 1.5*constants.U, 0;...
+    0.5*constants.U, 0, 0;...
+    0, -1.5*constants.U, 0;...
+    -0.5*constants.U, 0, 0;...
+    0, 0, constants.U;...
+    0, 0, -constants.U];
+    
+surfaces.panels.r_surf = spacecraft.r_CoM_panels;
 
 surfaces.cd = 2.2;
 surfaces.body.rhoS = 0;
@@ -112,4 +122,45 @@ surfaces.panels.rhoD = 0;
 
 %% Magnetic properties
 
-spacecraft.residual_dipole = [0.01; 0.01; 0.01]; %[A/m^2]
+spacecraft.res_dip = [0.01; 0.01; 0.01]; %[A/m^2]
+
+%% Orbit setup (RaInCube satellite, NORAD 43548, TLE @ 23/12/2020)
+
+rp = 400 + 6378.1; %[km]
+e = 0.0001982;
+ra = rp*(1+e)/(1-e);
+
+orbit.a0 = (ra + rp)/2;
+orbit.e0 = e;
+orbit.i0 = deg2rad(51.6133);
+orbit.OM0 = deg2rad(23.4318);
+orbit.om0 = deg2rad(43.8993);
+orbit.th0 = 0;
+
+%% Initial conditions
+
+orbit.kep0 = [orbit.a0; orbit.e0; orbit.i0; orbit.OM0; orbit.om0; orbit.th0];
+
+p = orbit.a0*(1-orbit.e0^2);
+r = p/(1+orbit.e0*cos(orbit.th0));
+
+rr = r*[cos(orbit.th0);sin(orbit.th0);0];
+vv = sqrt(constants.mu/p)*[-sin(orbit.th0);orbit.e0+cos(orbit.th0);0];
+
+ROM = [ cos(orbit.OM0) sin(orbit.OM0) 0; -sin(orbit.OM0) cos(orbit.OM0) 0; 0 0 1];
+Ri = [ 1 0 0; 0 cos(orbit.i0) sin(orbit.i0); 0 -sin(orbit.i0) cos(orbit.i0)];
+Rom = [ cos(orbit.om0) sin(orbit.om0) 0; -sin(orbit.om0) cos(orbit.om0) 0; 0 0 1];
+
+T = Rom*Ri*ROM;
+
+orbit.rr0 = T'*rr;
+orbit.vv0 = T'*vv;
+
+orbit.car0 = [orbit.rr0; orbit.vv0];
+
+spacecraft.w0 = deg2rad([10; 5; 3]);
+spacecraft.q0 = [0; 0; 0; 1];
+
+%% Clear variables
+
+clearvars -except constants spacecraft surfaces orbit
